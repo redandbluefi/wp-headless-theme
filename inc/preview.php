8b6c8ae1145edc47c1b86a181a081854b02d3a50
Hannu Kärkkäinen <hannu@redandblue.fi>
2018-06-13 13:17:54 +0300

    Modified preview link for post type 'post'

--- inc/preview.php
+++ inc/preview.php
@@ -7,13 +7,18 @@ function headless_post_preview_link($preview_link, $post) {
   $postStatus = get_post_status($post->ID);
   $lang = pll_current_language('slug');
   $nonce = wp_create_nonce('bodybuilder_page_preview');
+  $type = get_post_type($post);
+  $url = home_url().'/'.$lang;
+  if ($type === 'post') {
+    $url .= '/post';
+  }
 
   if ($postStatus != 'draft' && $postStatus != 'auto-draft') {
     // Preview URL for all published posts
-    return home_url().'/'.$lang.'/'.get_page_uri($post->ID);
+    return $url.'/'.get_page_uri($post->ID);
   } else {
     // Preview URL for all posts which are in draft
-    return home_url().'/'.$lang.'/?preview='.$post->ID.'&token='.$nonce;
+    return $url.'/?preview='.$post->ID.'&token='.$nonce;
   }
 }
 
